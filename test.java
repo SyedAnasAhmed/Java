88a6a9148b94f6737aa5542a4e51db5402553fd1
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2024-08-26 04:10:55 +0500

    new commit

--- test.java
+++ test.java
@@ -1,5 +1,70 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        //3.6 Q1
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the number of seconds");
+//        int user_input  = sc.nextInt();
+//
+//        int hours = user_input / 3600;
+//        int mins = (user_input % 3600) / 60;
+//        int secs = user_input % 60;
+//
+//        if (hours == 1){
+//            System.out.println("In " + user_input + " seconds, There is" + hours + " hour," + mins + " minutes and " + secs + " seconds");
+//        }
+//        else {
+//            System.out.println("In " + user_input + " seconds, There are " + hours + " hours," + mins + " minutes and " + secs + " seconds");
+//
+//        }
+
+        //3.6 Q2:
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a lower limit for the interval :  ");
+//        int lowerlimit = sc.nextInt();
+//        System.out.print("Enter a upper limit for the interval : ");
+//        int upperlimit = sc.nextInt();
+//        System.out.println("Now enter a number to check where it lies in the interval!");
+//        int userinput = sc.nextInt();
+//
+//        if (userinput > lowerlimit && userinput < upperlimit) {
+//            System.out.println("The value " + userinput + " lies within the interval : [" + lowerlimit + "," + upperlimit + "]");
+//        }
+//        else {
+//            System.out.println("The value " + userinput + " does not lie in the interval : [" + lowerlimit + "," + upperlimit + "]");
+//        }
+
+
+//        3.6 Q4
+
+//        Scanner sc  = new Scanner(System.in);
+//        System.out.println("How many numbers will you enter? ");
+//        int n = sc.nextInt();
+//        int sum=0;
+//        int i = 0;
+//
+//        do {
+//            System.out.println("Enter a number");
+//            int userinput = sc.nextInt();
+//            sum = sum + userinput;
+//            i++;
+//        }
+//        while (  i<n  );
+//
+//        System.out.println(sum);
+
+
+
+
+
+
+
+
+
+
+
     }
 }
